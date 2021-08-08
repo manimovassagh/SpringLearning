3f09f85760396975110bff8363a3fd11be82f082
manimovassagh <mani.mgh@gmail.com>
2021-08-08 15:49:01 +0200

    user class added

--- src/main/java/com/github/manimovassagh/springlearning/book/BookDaoService.java
+++ src/main/java/com/github/manimovassagh/springlearning/book/BookDaoService.java
@@ -5,10 +5,12 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 @Component
 public class BookDaoService {
-    private static List<Book> bookList=new ArrayList<Book>();
+    private static List<Book> bookList = new ArrayList<Book>();
 
     static {
         bookList.add(new Book("Zendegi Aram", new Date(), "Sahar Morattab"));
@@ -17,11 +19,20 @@ public class BookDaoService {
 
     }
 
-    public List<Book> findAllBooks(){
+    public List<Book> findAllBooks() {
         return bookList;
     }
 
 
+    public Book saveBook(Book book){
+        bookList.add(book);
+        return book;
+    }
+
+    public Book findOneBook(String bookName){
+        Stream<Book> bookResult = bookList.stream().filter(book -> book.getBookName().equals(bookName));
+        return (Book) bookResult;
+    }
 
 
 }
