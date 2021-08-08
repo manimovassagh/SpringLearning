dc1df31f9dce648fa8aabe5637954b67eeb05021
manimovassagh <mani.mgh@gmail.com>
2021-08-08 13:35:31 +0200

    33

--- src/main/java/com/github/manimovassagh/springlearning/LearnSpringApplication.java
+++ src/main/java/com/github/manimovassagh/springlearning/LearnSpringApplication.java
@@ -2,7 +2,7 @@ package com.github.manimovassagh.springlearning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//check
+
 @SpringBootApplication
 public class LearnSpringApplication {
 
