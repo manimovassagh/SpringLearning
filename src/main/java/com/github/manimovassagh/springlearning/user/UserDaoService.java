e479f2af6751407cff5f1025fe4783cec808155d
manimovassagh <mani.mgh@gmail.com>
2021-08-08 16:30:59 +0200

    just a service

---
+++ src/main/java/com/github/manimovassagh/springlearning/user/UserDaoService.java
@@ -0,0 +1,45 @@
+package com.github.manimovassagh.springlearning.user;
+
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class UserDaoService {
+
+    public static int userCount = 5;
+    public static List<User> users = new ArrayList<>();
+    static {
+        users.add(new User(1, "Sahar", new Date()));
+        users.add(new User(2, "Mani", new Date()));
+        users.add(new User(3, "Mehdi", new Date()));
+        users.add(new User(4, "David", new Date()));
+        users.add(new User(5, "Armin", new Date()));
+    }
+
+//Find All Users
+    public List<User> findAllUsers(){
+        return users;
+    }
+//Find Specific User
+    public User findOneUser(Integer id){
+       for (User user:users){
+        if (user.getId()==id){
+            return user;
+        }
+       }
+        return null;
+    }
+
+    //Save New User
+    public User addUser(User user){
+        if (user.getId()==0){
+            user.setId(++userCount);
+        }
+               users.add(user);
+        return user;
+    }
+}
